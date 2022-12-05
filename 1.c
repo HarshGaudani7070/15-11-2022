241cb6afd25dcaf48f7bc64152e7b389eeedb0ad
harshg2961 <118956982+harshg2961@users.noreply.github.com>
2022-12-05 12:05:30 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+main(){
+	
+	int x=10;
+	
+	int y=20;
+	
+	int z;
+	
+	z= (x*x)+ (y*y) + (2*x*y);
+	
+	printf("%i",z);
+}
---
+++ 4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+main(){
+	
+	int x=10;
+	
+	int y=20;
+	
+	int z;
+	
+	z= (x*x*x)- (y*y*y) - (3*x*x*y) + (3*x*y*y);
+	
+	printf("%i",z);
+}
